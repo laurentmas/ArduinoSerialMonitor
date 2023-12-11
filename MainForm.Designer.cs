475727fa53e2087a042d1a763b329b5553197d2b
Laurent Mas <laurent.mas@gmail.com>
2023-12-11 19:12:09 +0100

    Serial Monitor initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SerialMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.cboxComport = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cboxBaudrate = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tboxData = new System.Windows.Forms.TextBox();
+            this.tboxReceive = new System.Windows.Forms.TextBox();
+            this.btnClearOutput = new System.Windows.Forms.Button();
+            this.cboxLinetype = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboxComport
+            // 
+            this.cboxComport.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxComport.FormattingEnabled = true;
+            this.cboxComport.Location = new System.Drawing.Point(8, 11);
+            this.cboxComport.Name = "cboxComport";
+            this.cboxComport.Size = new System.Drawing.Size(272, 21);
+            this.cboxComport.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(373, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cboxBaudrate
+            // 
+            this.cboxBaudrate.AllowDrop = true;
+            this.cboxBaudrate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboxBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxBaudrate.Location = new System.Drawing.Point(261, 278);
+            this.cboxBaudrate.Name = "cboxBaudrate";
+            this.cboxBaudrate.Size = new System.Drawing.Size(103, 21);
+            this.cboxBaudrate.TabIndex = 7;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(289, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(373, 42);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tboxData
+            // 
+            this.tboxData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxData.Location = new System.Drawing.Point(8, 44);
+            this.tboxData.Name = "tboxData";
+            this.tboxData.Size = new System.Drawing.Size(356, 20);
+            this.tboxData.TabIndex = 3;
+            this.tboxData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tboxData_KeyPress);
+            // 
+            // tboxReceive
+            // 
+            this.tboxReceive.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tboxReceive.BackColor = System.Drawing.SystemColors.Window;
+            this.tboxReceive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxReceive.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxReceive.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tboxReceive.Location = new System.Drawing.Point(8, 71);
+            this.tboxReceive.Multiline = true;
+            this.tboxReceive.Name = "tboxReceive";
+            this.tboxReceive.ReadOnly = true;
+            this.tboxReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tboxReceive.Size = new System.Drawing.Size(440, 199);
+            this.tboxReceive.TabIndex = 5;
+            // 
+            // btnClearOutput
+            // 
+            this.btnClearOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearOutput.Location = new System.Drawing.Point(373, 277);
+            this.btnClearOutput.Name = "btnClearOutput";
+            this.btnClearOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnClearOutput.TabIndex = 8;
+            this.btnClearOutput.Text = "Clear output";
+            this.btnClearOutput.UseVisualStyleBackColor = true;
+            this.btnClearOutput.Click += new System.EventHandler(this.btnClearOutput_Click);
+            // 
+            // cboxLinetype
+            // 
+            this.cboxLinetype.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboxLinetype.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxLinetype.FormattingEnabled = true;
+            this.cboxLinetype.Location = new System.Drawing.Point(130, 278);
+            this.cboxLinetype.Name = "cboxLinetype";
+            this.cboxLinetype.Size = new System.Drawing.Size(121, 21);
+            this.cboxLinetype.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 308);
+            this.Controls.Add(this.cboxLinetype);
+            this.Controls.Add(this.btnClearOutput);
+            this.Controls.Add(this.tboxReceive);
+            this.Controls.Add(this.tboxData);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cboxBaudrate);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cboxComport);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(472, 347);
+            this.Name = "MainForm";
+            this.Text = "Arduino Serial Monitor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboxComport;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cboxBaudrate;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tboxData;
+        private System.Windows.Forms.TextBox tboxReceive;
+        private System.Windows.Forms.Button btnClearOutput;
+        private System.Windows.Forms.ComboBox cboxLinetype;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SerialMonitor
+{
+    public partial class MainForm : Form
+    {
+        #region Constant
+        private readonly int[] baudrate = { 9600, 19200, 38400, 115200, 230400, 460800, 921600, 3860000 };
+        private readonly string[] lineTypes = { "No Line Ending", "NewLine", "Carriage return", "Both NL & CR"};
+        private readonly string defaultBaudrate = "115200 baud";
+        private readonly string defaultLinetype = "NewLine";
+        #endregion
+
+        private SerialPort Serial = new SerialPort();
+
+        #region Local Helpers
+        private void UpdateCOMPortList()
+        {
+            // Get all existing Com Port names
+            string[] Ports = System.IO.Ports.SerialPort.GetPortNames();
+            string selectDefaultPort = "";
+            cboxComport.Items.Clear();
+            cboxBaudrate.Items.Clear();
+            cboxLinetype.Items.Clear();
+
+            // Append existing COM to the cboxComport list
+            foreach (var item in Ports)
+            {
+                cboxComport.Items.Add(item);
+                selectDefaultPort = item;
+            }
+
+            //select lastest Port found by default
+            if(Ports.Length > 1)
+            {
+                cboxComport.Text = selectDefaultPort;
+            }
+            
+
+            // Append possible Baudrate to the cboxBaudrate list
+            foreach (var baud in baudrate)
+            {
+                cboxBaudrate.Items.Add(baud.ToString()+" baud");
+            }
+            //set default value
+            cboxBaudrate.Text = defaultBaudrate;
+
+            
+            // Append possible Baudrate to the cboxBaudrate list
+            foreach (var line in lineTypes)
+            {
+                cboxLinetype.Items.Add(line.ToString());
+            }
+            //set default value
+            cboxLinetype.Text = defaultLinetype;
+        }
+        #endregion
+
+        #region Delegates
+        public delegate void UPDATE_OUTPUT_TEXT(String Str);
+        public void UpdateOutputText(String Str)
+        {
+            tboxReceive.Text += Str;
+            tboxReceive.SelectionStart = tboxReceive.Text.Length;
+            tboxReceive.ScrollToCaret();
+        }
+        #endregion
+
+
+        #region Handlers
+        void SerialOnReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            String str = Serial.ReadExisting();
+            
+           Invoke(new UPDATE_OUTPUT_TEXT(UpdateOutputText), str);
+        }
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // If user click disconnect
+            if ("Disconnect" == btnConnect.Text.ToString())
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Close();
+                }
+
+                btnConnect.Text = "Connect";
+                cboxComport.Enabled = true;
+                cboxBaudrate.Enabled = true;
+                btnRefresh.Enabled = true;
+
+                return;
+            }
+
+            // else we gonna open the desired COM port
+            // Get user comport from cbox
+            try
+            {
+                Serial.PortName = cboxComport.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Error! No COM Port selected");
+                return;
+            }
+
+            // Get user baudrate from cbox
+            try
+            {
+                Serial.BaudRate = int.Parse(cboxBaudrate.Text.ToString().Replace(" baud",""));
+            }
+            catch
+            {
+                MessageBox.Show("Error! No Baudrate selected");
+                return;
+            }
+
+            // Serial Port Configuration
+            Serial.Parity = Parity.None;
+            Serial.DataBits = 8;
+            Serial.ReceivedBytesThreshold = 1;
+            Serial.StopBits = StopBits.One;
+            Serial.Handshake = Handshake.None;
+            Serial.WriteTimeout = 3000;
+            Serial.Encoding = Encoding.GetEncoding("utf-8");
+            //Serial.RtsEnable = true;
+            Serial.DtrEnable = true;
+
+
+            // Check if com port is opened by other application
+            if (false == Serial.IsOpen)
+            {
+                try
+                {
+                    // Com port available
+                    Serial.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("The COM port is not accessible", "Error");
+                    return;
+                };
+
+
+                // double comform it is opened
+                if (true == Serial.IsOpen)
+                {
+                    btnConnect.Text = "Disconnect";
+                    cboxComport.Enabled = false;
+                    cboxBaudrate.Enabled = false;
+                    btnRefresh.Enabled = false;
+
+                    // Add callback handler for receiving
+                    Serial.DataReceived += new SerialDataReceivedEventHandler(SerialOnReceivedHandler);
+
+                }
+
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // We need to populate the lists during mainform is loading
+            UpdateCOMPortList();            
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            // We need to update all lists again if user requested
+            UpdateCOMPortList();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendSerialCommand();
+        }
+
+        private void SendSerialCommand()
+        {
+            if (null != Serial)
+            {
+                if (true == Serial.IsOpen)
+                {
+                    Serial.Write(tboxData.Text + NewLineCode());
+                    tboxData.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("COM Port is not Opened");
+                }
+            }
+        }
+
+        private string NewLineCode()
+        {
+            string newlinechar = "";
+
+            switch(cboxLinetype.Text)
+            {
+                case "No Line Ending":
+                    newlinechar = "";
+                    break;
+                case "NewLine":
+                    newlinechar = "\n";
+                    break;
+                case "Carriage return":
+                    newlinechar = "\r";
+                    break;
+                case "Both NL & CR":
+                    newlinechar = "\n\r";
+                    break;
+            }
+
+            return newlinechar;
+        }
+
+        private void btnClearOutput_Click(object sender, EventArgs e)
+        {
+            tboxReceive.Clear();
+        }
+
+        private void tboxData_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Return))
+            {
+                SendSerialCommand();
+            }
+        }
+    }
+}
